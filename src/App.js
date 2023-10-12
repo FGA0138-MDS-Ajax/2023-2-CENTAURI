83d00b5287ff1f4e19566e9a952433ed2d1b5f46
Danielle Rodrigues <Daniellers2003@gmail.com>
2023-10-12 00:33:31 -0300

    Inicio da tela de login
    Criação da tela de login :)
    
    Co-Authored-By: Renan Araújo <111506459+renantfm4@users.noreply.github.com>
    Co-Authored-By: felipegf1 <89036880+felipegf1@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Login from './components/pages/Login';
+import Error from './components/pages/Error';
+import Pesquisa from './components/pages/Pesquisa';
+import Usuario from './components/pages/Usuario';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/login' element={<Login/>}/>
+          <Route exact path='/error' element={<Error/>}/>
+          <Route exact path='/pesquisa' element={<Pesquisa/>}/>
+          <Route exact path='/usuario' element={<Usuario/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/pages/Usuario.js
@@ -0,0 +1,7 @@
+function Usuario(){
+    return(
+        <h1>Usuario</h1>
+    )
+}
+
+export default Usuario;
