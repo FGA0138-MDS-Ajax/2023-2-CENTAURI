ff82430fce340d4741a0c7c6f6ee5d70fe8d3798
Danielle Rodrigues <Daniellers2003@gmail.com>
2023-10-18 18:11:07 -0300

    Página HOME
    Co-Authored-By: felipegf1 <89036880+felipegf1@users.noreply.github.com>
    Co-Authored-By: Renan Araújo <111506459+renantfm4@users.noreply.github.com>

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,35 @@
 import styles from "./Home.module.css"
-import search from '../img/search.png'
+import botaoPesquisa from '../img/botaoPesquisa.svg'
+import logo from "../img/logo.svg"
+import botaoPlus from "../img/Plus Button.svg"
+import onda from "../img/Rectangle9.svg"
+import menina from "../img/meninaNotebook.svg"
+import fundo from "../img/fundoMenina.svg"
 
 function Home(){
     return(
         <div className={styles.home_container}>
+            <button className = {styles.botaoLogin}>LOGIN</button>
+            <img src = {logo} className={styles.logo}></img>
             <form action="" className={styles.search_bar}>
             <input type="text" placeholder="Faça sua pesquisa"/>
-            <button type="submit"><img src={search}></img></button>
+            <button type="submit" className={styles.botaoPesquisa}><img src={botaoPesquisa}></img></button>
             </form>
+            <div className = {styles.h}>
+                <h1>Confira mais sobre o Projeto</h1>
+                <button type="submit"><img src={botaoPlus}></img></button>               
+            </div>
+            <div>  
+                <img src={onda} className={styles.onda}></img>
+                <img src={fundo} className={styles.fundo}></img>
+                <img src = {menina} className={styles.menina}></img>
+                <h1 className={styles.titulo}>Sobre o UnBuscas</h1>
+                <p className={styles.texto}>Um recurso de pesquisa rápida<br />
+                    feita por estudantes da UnB<br />
+                    para os próprios estudantes da UnB</p>
+                <p className={styles.texto2}>A plataforma web UnBuscas tem como seu principal<br /> 
+                    objetivo buscar documentos da UnB de forma rápida e eficiente</p>
+            </div>
         </div>
     )
 }
