2b31cfd212fec2b45007d2665afcd0ae3c6304a4
Renan Araújo <111506459+renantfm4@users.noreply.github.com>
2023-11-12 15:48:11 -0300

    Filtros

--- src/components/pages/Pesquisa.js
+++ src/components/pages/Pesquisa.js
@@ -1,58 +1,155 @@
-import { useState } from 'react'
-import styles from "./Pesquisa.module.css"
-const { MeiliSearch } = require('meilisearch')
+import React, { useState, useEffect } from 'react';
+import styles from "./Pesquisa.module.css";
+const { MeiliSearch } = require('meilisearch');
 
+function Pesquisa() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [originalResults, setOriginalResults] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [sortOption, setSortOption] = useState('relevance');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [periodDropdownOpen, setPeriodDropdownOpen] = useState(false);
+  const [selectedType, setSelectedType] = useState(''); // Adicionado estado para o tipo
 
-function Pesquisa(){
-    const [searchResults, setSearchResults] = useState([]);
-    // const [order, setOrder] = useState(1);
-    // const [collumnOrder, setColumnOrder] = useState('resource.date');
+  const client = new MeiliSearch({
+    host: 'http://127.0.0.1:7700',
+    apiKey: 'aSampleMasterKey',
+  });
 
-    // const handleOrder = date => {
-    //     setOrder(order)
-    //     setColumnOrder(date)
-    // }
+  const searchDocuments = async (query) => {
+    client
+      .index("entries")
+      .search(query)
+      .then((results) => {
+        const hits = results.hits;
+        setSearchResults(hits);
+        setOriginalResults(hits);
+        setFilteredResults(hits);
+      });
+  };
 
-    // searchResults = searchResults.sort((a, b) => {
-    //     return a[collumnOrder] < b[collumnOrder] ? -order : order;
-    // })
+  const sortResults = () => {
+    switch (sortOption) {
+      case 'newest':
+        setFilteredResults([...filteredResults].sort((a, b) => b.date - a.date));
+        break;
+      case 'oldest':
+        setFilteredResults([...filteredResults].sort((a, b) => a.date - b.date));
+        break;
+      case 'relevance':
+      default:
+        setFilteredResults([...originalResults]);
+        break;
+    }
+  };
 
-    const client = new MeiliSearch({
-      host: 'http://127.0.0.1:7700',
-      apiKey: 'aSampleMasterKey',
-    })
+  const filterResults = () => {
+    let resultsToFilter = [...originalResults];
 
-    const searchDocuments = async (e)=> {
-        client
-            .index("entries")
-            .search(e.target.value)
-            .then((results) => {
-                setSearchResults(results.hits);
-        });
-    };
+    // Filtrar por tipo se selecionado
+    if (selectedType) {
+      resultsToFilter = resultsToFilter.filter(entry => entry.type === selectedType);
+    }
+
+    // Filtrar por data se disponível
+    if (startDate && endDate) {
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+      resultsToFilter = resultsToFilter.filter(entry => {
+        const entryDate = new Date(entry.date * 1000);
+        return entryDate >= start && entryDate <= end;
+      });
+    }
+
+    setFilteredResults(resultsToFilter);
+  };
+
+  useEffect(() => {
+    sortResults();
+  }, [sortOption]);
+
+  useEffect(() => {
+    filterResults();
+  }, [startDate, endDate, selectedType]);
+
+  const handleSortOptionChange = (option) => {
+    setSortOption(option);
+  };
+
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+  };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date);
+  };
+
+  const handleTogglePeriodDropdown = () => {
+    setPeriodDropdownOpen(!periodDropdownOpen);
+  };
+
+  const handleTypeChange = (type) => {
+    setSelectedType(type);
+  };
+
   return (
-      <div className={styles.search_container}>
-        <form className={styles.search_bar}>
-            <input 
-            
-            type="text" 
-            placeholder="Pesquise um documento aqui"
-            onChange={(e) => searchDocuments(e)}/> 
-            {/* <button type="submit" onClick={(e) => handleOrder(e)}>buscar</button> */}
-        </form>
-        
-        <div className={styles.grid}>
-            {searchResults.map((resource) => {
-                return <tr key={resource.id}>
-                        <td><h3>{resource.title}</h3></td>
-                        <td><p>{resource.content.substring(0, 400)}...</p></td>
-                        <td><a href={resource.link}>Baixe aqui o documento</a>
-                        <br/></td>
-                    </tr>
-            }
-            )};
+    <div className={styles.search_container}>
+      <form className={styles.search_bar}>
+        <input
+          type="text"
+          placeholder="Pesquise um documento aqui"
+          onChange={(e) => searchDocuments(e.target.value)}
+        />
+      </form>
+
+      {/* Add UI for sorting and filtering */}
+      <div className={styles.sortingFilteringControls}>
+        {/* Dropdown para selecionar a opção de ordenação */}
+        <select value={sortOption} onChange={(e) => handleSortOptionChange(e.target.value)}>
+          <option value="relevance">Relevância</option>
+          <option value="newest">Mais recentes</option>
+          <option value="oldest">Mais antigos</option>
+        </select>
+
+        {/* Dropdown para o período */}
+        <div className={styles.periodDropdown}>
+          <button type="button" onClick={handleTogglePeriodDropdown}>
+            Período
+          </button>
+          {periodDropdownOpen && (
+            <div className={styles.periodDropdownContent}>
+              <label>Start Date:</label>
+              <input type="date" value={startDate} onChange={(e) => handleStartDateChange(e.target.value)} />
+
+              <label>End Date:</label>
+              <input type="date" value={endDate} onChange={(e) => handleEndDateChange(e.target.value)} />
             </div>
+          )}
         </div>
-)};
+
+        {/* Filtro por tipo */}
+        <div>
+          <select value={selectedType} onChange={(e) => handleTypeChange(e.target.value)}>
+            <option value="">Todos</option>
+            <option value="Normativo">Normativo</option>
+            <option value="Deliberativo">Deliberativo</option>
+          </select>
+        </div>
+      </div>
+
+      <div className={styles.grid}>
+        {/* Display search results */}
+        {filteredResults.map((resource) => (
+          <tr key={resource.id}>
+            <td><h3>{resource.title}</h3></td>
+            <td><p>{resource.content.substring(0, 400)}...</p></td>
+            <td><a href={resource.link}>Baixe aqui o documento</a><br /></td>
+          </tr>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default Pesquisa;
