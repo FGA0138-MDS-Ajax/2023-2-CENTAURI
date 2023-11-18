97f93a7fded6d8896901a22a1b283256c0a43eac
Marianannn <mariana.leticia05@gmail.com>
2023-11-18 20:30:11 -0300

    mudança em um codigo

--- back-usuarios/routers/auth/passport.js
+++ back-usuarios/routers/auth/passport.js
@@ -79,7 +79,7 @@ router.get("/google/callback", passport.authenticate("google",), (req, res) => {
     if (req.user) {
         console.log("the use is", req.user[0]); //Just for debugging
         //Creating a unique token using sign method which is provided by JWT, remember the 2nd parameter should be a secret key and that should have atleast length of 20, i have just passed 'rahulnikam' but you should not do the same and this should be kept in environment variable so that no one can see it
-        const googleAuthToken = jwt.sign({ googleAuthToken: req.user[0].googleId }, "AIzaSyBkWXxqi9VUKpzMWjeSKz_8Ft_eiGLIQfc", { expiresIn: 86400000 })
+        const googleAuthToken = jwt.sign({ googleAuthToken: req.user[0].googleId }, "batataQUENTEQUENTEQUENTEQUEIMOU", { expiresIn: 86400000 })
         //res.cookie will set a cookie in user's header (i mean in users http header😂)
         // we are saying that create a cookie with a name of googleAuthToken and we are passing the token that we generated on line no 80, and the 3rd parameter is the expire of that cookie.
         res.cookie("googleAuthToken", googleAuthToken, { expires: new Date(Date.now() + 86400 * 1000), httpOnly: true })
