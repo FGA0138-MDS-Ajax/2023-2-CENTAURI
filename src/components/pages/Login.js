5799aecff90504b34578e2000428f741ce8e3b36
Renan Araújo <111506459+renantfm4@users.noreply.github.com>
2023-11-25 15:38:04 -0300

    att

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -9,8 +9,6 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from './AuthContext';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const navigate = useNavigate();
   const { login } = useAuth();
 
