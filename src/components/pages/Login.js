9b4c19cc9bab19a84e8d61d39165c10fe4426a91
Marianannn <mariana.leticia05@gmail.com>
2023-11-26 19:15:27 -0300

    tentando revolver problemas do conseguir redirecionar as páginas mas não conseguir adicionar no banco de dados
    Co-authored-by: renantfm4 <renantfm0@gmail.com>

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -7,6 +7,8 @@ import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from './AuthContext';
+import Axios from "axios";
+import React from "react";
 
 function Login() {
   const navigate = useNavigate();
@@ -35,6 +37,26 @@ function Login() {
     navigate("/");
   }
 
+  const signInWithGoogle = (credentialResponse) => {
+    
+    window.open("http://localhost:8800/auth/google", "_self", 'toolbar=no, scrollbars=yes, resizable=no, width=1000, height=auto')
+
+  }
+
+  React.useEffect(() => {
+    Axios.get("http://localhost:8800/auth/login/success", {
+      withCredentials: true,
+    })
+      .then((res) => {
+        if (res.status == 200) {
+          navigate('/login')
+        } else {
+          console.log("No status");
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className={styles.login_container}>
       
@@ -44,7 +66,10 @@ function Login() {
       <div className={styles.c1}>
         <h1>Entre na sua conta</h1>
         <hr />
-        <GoogleOAuthProvider clientId="829989984645-1n9j5s35r34n85fkj83hjiiisc6jg20e.apps.googleusercontent.com">
+
+        
+
+        {/* <GoogleOAuthProvider clientId="829989984645-1n9j5s35r34n85fkj83hjiiisc6jg20e.apps.googleusercontent.com">
           <GoogleLogin
             onSuccess={handleLoginSuccess}
             onError={() => {
@@ -52,7 +77,11 @@ function Login() {
             }}
           />
         </GoogleOAuthProvider>
+        <button>
+            <a className="botao_google" href="http://localhost:8800/auth/google/callback" >Login with Google</a>
+        </button> */}
       </div>
+      <button onClick={signInWithGoogle}>logue aq</button>
     </div>
   );
 }
