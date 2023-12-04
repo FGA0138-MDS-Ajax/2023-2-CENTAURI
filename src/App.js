a5746ec57c7cb82b79bd7b607866f090ca8ef5fa
Marianannn <mariana.leticia05@gmail.com>
2023-12-03 23:45:07 -0300

    Ajuste de rota

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Login from './components/pages/Login';
 import Error from './components/pages/Error';
 import Pesquisa from './components/pages/Pesquisa';
 import Usuario from './components/pages/Usuario';
-import Favoritos from './components/pages/favoritos'
 
 import { AuthProvider } from './components/contexts/AuthContext';
 // import FavoritesProvider from './contexts/Favorites';
@@ -20,7 +19,6 @@ function App() {
               <Route exact path='/error' element={<Error />} />
               <Route exact path='/pesquisa' element={<Pesquisa />} />
               <Route exact path='/usuario' element={<Usuario />} />
-              <Route exact path='/favoritos' element={<Favoritos />} />
             </Routes>
           </Router>
       </AuthProvider>
